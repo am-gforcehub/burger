a5fc503a73bae70933406dc5b0ff5f4358de9aa6
am-gforce <47460424+am-gforcehub@users.noreply.github.com>
2019-06-10 18:21:18 -0500

    added logic

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Double-Bacon Cheeseburger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('BBQ-Bacon Cheeseburger');
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Cheeseburger', true);
+
